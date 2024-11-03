2ffc02f33bfbf5b3b3385c4f93956e23b2812655
BlinhNhi <trantoan1682016@gmail.com>
2024-11-03 13:05:15 +0700

    CRUD Product

--- back_end/back_end/Controllers/ProductController.cs
+++ back_end/back_end/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using back_end.IRepository;
 using back_end.Models;
 using back_end.ReponseData;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,16 +38,78 @@ namespace back_end.Controllers
             }
         }
 
+
+        [AllowAnonymous]
+        [HttpGet("{Id}")]
+        public async Task<ActionResult> getAProductById(Guid Id)
+        {
+            try
+            {
+                var list = await repo.GetProductById(Id);
+                if (list != null)
+                {
+                    var response = new ResponseData<IEnumerable<Product>>(StatusCodes.Status200OK, "Get Product successfully", list, null);
+                    return Ok(response);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
         [HttpPost]
         public async Task<ActionResult> CreateProduct([FromForm] Product product)
         {
             try
             {
-
                 var list = await repo.CreateProduct(product);
                 if (list == true)
                 {
                     var response = new ResponseData<Product>(StatusCodes.Status200OK, "Create new product successfully", product, null);
+                    return Ok(response);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<ActionResult> PutProduct(Guid Id, [FromForm] Product product)
+        {
+            try
+            {
+                bool list = await repo.PutProduct(Id, product);
+                if (list)
+                {
+                    var response = new ResponseData<Product>(StatusCodes.Status200OK, "Edit Product Successfully", product, null);
+                    return Ok(response);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> DeleteProduct(Guid Id)
+        {
+            try
+            {
+                var list = await repo.DeleteProduct(Id);
+                if (list != null)
+                {
+                    var response = new ResponseData<Product>(StatusCodes.Status200OK, "Delete Product successfully", list, null);
                     return Ok(response);
                 }
                 return BadRequest();
--- back_end/back_end/Services/ProductService.cs
+++ back_end/back_end/Services/ProductService.cs
@@ -2,6 +2,7 @@
 using back_end.Models;
 using Microsoft.EntityFrameworkCore;
 using MySqlX.XDevAPI.Common;
+using System;
 
 namespace back_end.Services
 {
@@ -26,13 +27,14 @@ namespace back_end.Services
                 {
                     Id = p.Id,
                     NameProduct = p.NameProduct,
+                    PriceProduct = p.PriceProduct,
                     StockQuantity = p.StockQuantity,
                     NumberOfProductSold = p.NumberOfProductSold,
                     NumberOfProductInStock = p.NumberOfProductInStock,
                     ImageProduct = p.ImageProduct,
-                    ColorId = p.ColorId,
+                   /* ColorId = p.ColorId,*/
                     Colors = p.Colors, // Lấy Colors liên kết
-                    SizeId = p.SizeId,
+                  /*  SizeId = p.SizeId,*/
                     Sizes = p.Sizes, // Lấy Sizes liên kết
                     Category = new Category() // Gán Category
                     {
@@ -84,20 +86,58 @@ namespace back_end.Services
         }
         public async Task<IEnumerable<Product>> GetProductById(Guid Id)
         {
-            return await db.Products.Where(p => p.Id == Id).ToListAsync();
+            return await db.Products.Include(c=>c.Category).Include(co=>co.Colors).Where(p=>p.Id == Id).Include(si=>si.Sizes).Where(p=>p.Id == Id).ToListAsync();
         }
 
         public async Task<bool> PutProduct(Guid Id, Product product)
         {
-            var ExistingProduct = await db.Products.FindAsync(Id);
+            var ExistingProduct = await db.Products.SingleOrDefaultAsync(p=>p.Id == Id);
             if (ExistingProduct != null)
             {
+                Category category = db.Categories.Where(ca => ca.Id == product.CategoryId).FirstOrDefault();
+
                 ExistingProduct.PriceProduct = product.PriceProduct;
                 ExistingProduct.NameProduct = product.NameProduct;
                 ExistingProduct.StockQuantity = product.StockQuantity;
                 ExistingProduct.NumberOfProductSold = product.NumberOfProductSold;
                 ExistingProduct.NumberOfProductInStock = product.NumberOfProductInStock;
-                /*ExistingProduct.NumberOfProductInStock = product.NumberOfProductInStock;*/
+                ExistingProduct.Category = category;
+
+                var color = await db.ProductColors.Where(pc => pc.ProductId == Id).ToListAsync();
+                foreach (var i in color)
+                {   
+                    db.ProductColors.Remove(i);
+                    await db.SaveChangesAsync();
+                }
+                var ProductID = Id;
+                if(product.ColorId != null)
+                {
+                    List<string> list = product.ColorId.Split(",").ToList();
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        int id = Int32.Parse(list[i]);
+                        ProductColor newColor = new ProductColor { ProductId = ProductID, ColorId = id };
+                        db.ProductColors.Add(newColor);
+                        await db.SaveChangesAsync();
+                    }
+                }
+                var size = await db.ProductSizes.Where(pc => pc.ProductId == Id).ToListAsync();
+                foreach (var i in size)
+                {
+                    db.ProductSizes.Remove(i);
+                    await db.SaveChangesAsync();
+                }
+                if (product.SizeId != null)
+                {
+                    List<string> list = product.SizeId.Split(",").ToList();
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        int id = Int32.Parse(list[i]);
+                        ProductSize newSize = new ProductSize { ProductId = ProductID, SizeId = id };
+                        db.ProductSizes.Add(newSize);
+                        await db.SaveChangesAsync();
+                    }
+                }
                 await db.SaveChangesAsync();
                 return true;
             }
@@ -106,7 +146,7 @@ namespace back_end.Services
 
         public async Task<Product> DeleteProduct(Guid Id)
         {
-            var ExistingProduct = await db.Products.SingleOrDefaultAsync(x => x.Id == Id);
+            var ExistingProduct = await db.Products.SingleOrDefaultAsync(p => p.Id == Id);
             if (ExistingProduct != null)
             {
                 db.Products.Remove(ExistingProduct);
