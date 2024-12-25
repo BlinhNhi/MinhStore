9a1f13fc7f84822755ae18a98b426adfe426f6e9
BlinhNhi <trantoan1682016@gmail.com>
2024-12-26 00:57:34 +0700

    update client order for user

--- front_end/src/pages/user_page/CartShoppingUser/CartShoppingUser.js
+++ front_end/src/pages/user_page/CartShoppingUser/CartShoppingUser.js
@@ -34,25 +34,54 @@ function CartShoppingUser() {
     const columnsDataTest = [
         {
             title: 'Sản Phẩm',
-            dataIndex: 'name',
-            key: 'name',
+            render: (text, cat) => {
+                const productName = text?.products?.[0]?.nameProduct;
+                return (
+                    <>
+                        {productName || "Không có tên sản phẩm"}
+                    </>
+                );
+            }
         },
         {
-            dataIndex: 'image',
+            title: 'Hình Ảnh',
             width: '10%',
-            render: () => {
-                return <div className='flex flex-col items-center'><img src={NoImage} alt='no-image' className='w-[50px] h-[50px] object-cover border-2 rounded-lg' /></div>
+            render: (text) => {
+                // Chuyển chuỗi JSON thành mảng
+                const images = text?.products?.[0]?.imagesProduct ? JSON.parse(text.products[0].imagesProduct) : [];
+                const imageUrl = images?.[0] || NoImage; // Lấy URL đầu tiên hoặc sử dụng ảnh mặc định
+                return (
+                    <div className="flex flex-col items-center">
+                        <img
+                            src={imageUrl}
+                            alt="product-image"
+                            className="w-[50px] h-[50px] object-cover border-2 rounded-lg"
+                        />
+                    </div>
+                );
             }
         },
         {
             title: 'Giá',
-            dataIndex: 'price',
-            key: 'price',
+            render: (text, cat) => {
+                const productPrice = text?.products?.[0]?.priceProduct;
+                return (
+                    <>
+                        {productPrice || "Không có giá sản phẩm"} vnd
+                    </>
+                );
+            }
         },
         {
             title: 'Số Lượng',
-            dataIndex: 'quantityOrder',
-            key: 'quantityOrder',
+            width: '15%',
+            render: (text, cat) => {
+                return <>
+                    <Button key={1} href={`/admin/categories-mng/edit/${cat.id}`} type="link" onClick={() => {
+                    }}>{text?.quantityOrder}</Button>
+                </>
+
+            }
         },
         {
             title: 'Tạm Tính',
@@ -61,25 +90,24 @@ function CartShoppingUser() {
         },
         {
             title: 'Size',
-            dataIndex: 'size',
-            key: 'size',
+            render: (text, cat) => {
+                return (
+                    <>
+                        {text?.size?.numberOfSize || 'Không có size'}
+                    </>
+                );
+            }
         },
         {
             title: 'Màu Sắc',
-            dataIndex: 'color',
-            key: 'color',
-        },
-        {
-            title: 'Số Lượng',
-            width: '15%',
-            render: (text, cat) => {
-                return <>
-                    <Button key={1} href={`/admin/categories-mng/edit/${cat.id}`} type="link" onClick={() => {
-                    }}>1</Button>
-                </>
-
+            render: (text) => {
+                return (
+                    <>
+                        {text?.color?.name || 'Không có màu'}
+                    </>
+                );
             }
-        }
+        },
     ];
     return (
         <div className="">
@@ -91,8 +119,23 @@ function CartShoppingUser() {
                 <div className="">
                     <div className="py-8 px-4 rounded-md border-4 border-gray-300 flex flex-col gap-4">
                         <h1 className="text-gray-600 font-bold text-xl dark:text-gray-300 border-b-2 border-gray-200 pb-2">Cộng Giỏ Hàng</h1>
-                        <div className="flex gap-10 items-center border-b-2 border-gray-200 pb-2"><h3 className="text-base font-bold text-gray-600 dark:text-gray-300">Tạm Tính</h3><h4 className="text-base font-medium text-gray-500 dark:text-gray-200">1,870,000₫</h4></div>
-                        <div className="flex gap-10 items-center mb-2"><h3 className="text-base font-bold text-gray-600 dark:text-gray-300">Tổng </h3><h4 className="text-lg font-bold text-gray-500 dark:text-gray-200">1,870,000₫</h4></div>
+
+                        {
+                            orderDetailByUserId?.map((item, i) => {
+                                return (
+                                    <div className="flex gap-10 items-center border-b-2 border-gray-200 pb-2">
+                                        <h3 className="text-base font-bold text-gray-600 dark:text-gray-300">Tạm Tính</h3>
+                                        <h4 className="text-base font-medium text-gray-500 dark:text-gray-200">{item?.totalAmount}đ</h4>
+                                    </div>
+                                )
+                            })
+                        }
+                        <div className="flex gap-10 items-center mb-2"><h3 className="text-base font-bold text-gray-600 dark:text-gray-300">Tổng</h3> <h4 className="text-lg font-bold text-gray-500 dark:text-gray-200">
+                            {
+                                orderDetailByUserId?.reduce((total, item) => total + (item?.totalAmount || 0), 0).toLocaleString('vi-VN') + 'đ'
+                            }
+
+                        </h4></div>
                         <button className="p-2 text-gray-200 font-semibold text-base hover:bg-primary dark:bg-white dark:text-gray-400 dark:hover:bg-primary dark:hover:text-gray-100 rounded-md bg-black">Tiến Hành Thanh Toán</button>
                     </div>
                 </div>
