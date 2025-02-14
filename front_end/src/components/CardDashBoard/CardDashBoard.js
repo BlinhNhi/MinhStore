27379dedd34d6b0d7190c8dc0dc38c8d15f7938c
BlinhNhi <trantoan1682016@gmail.com>
2025-02-15 00:37:11 +0700

    fix calculate all products

--- front_end/src/components/CardDashBoard/CardDashBoard.js
+++ front_end/src/components/CardDashBoard/CardDashBoard.js
@@ -6,11 +6,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getListPaymentAction } from "../../redux_store/actions/PaymentAction";
 import { getListUserAction } from "../../redux_store/actions/UserAction";
+import { getListProductsAction } from "../../redux_store/actions/ProductAcction";
+
 
 function CardDashBoard() {
     const dispatch = useDispatch();
     let { arrPayments } = useSelector(state => state.PaymentReducer);
     let { arrUser } = useSelector(state => state.UserReducer)
+    let { arrProducts } = useSelector(state => state.ProductReducer)
+
     useEffect(() => {
         dispatch(getListPaymentAction());
         dispatch(getListUserAction());
@@ -20,18 +24,27 @@ function CardDashBoard() {
         ?.filter(order => order.statusOrder === 4)
         ?.reduce((sum, order) => sum + order.totalAmountOfOrder, 0) / 1000;
     const countOrder = arrPayments?.filter(order => order?.statusOrder === 4)?.length;
-    const totalProductInStock = arrPayments?.reduce((total, userOrder) => {
-        return total + userOrder.orders.reduce((orderTotal, order) => {
-            return orderTotal + order.products.reduce((productTotal, product) => {
-                return productTotal + product.numberOfProductInStock;
-            }, 0);
-        }, 0);
+
+    // const totalProductInStock = arrPayments?.reduce((total, userOrder) => {
+    //     return total + userOrder.orders.reduce((orderTotal, order) => {
+    //         return orderTotal + order.products.reduce((productTotal, product) => {
+    //             return productTotal + product.numberOfProductInStock;
+    //         }, 0);
+    //     }, 0);
+    // }, 0);
+    // Tính tổng numberOfProductInStock
+    const totalProductInStock = arrProducts?.reduce((total, product) => {
+        return total + product.numberOfProductInStock;
     }, 0);
+
+    console.log("Tổng số sản phẩm trong kho:", totalProductInStock);
 
     console.log("Tổng numberOfProductInStock:", totalProductInStock);
     console.log(arrPayments);
     console.log(countOrder);
     console.log(totalAmount);
+    console.log(arrProducts);
+
     return (
         <div>
             <div className="grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-2 place-items-start gap-4 mt-4" >
@@ -73,7 +86,7 @@ function CardDashBoard() {
                     <div className="flex flex-col gap-2 text-gray-500 px-6 py-2 ">
                         <h2 className="font-medium text-base sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg uppercase text-ellipsis overflow-hidden line-clamp-1">Sản Phẩm Còn Lại</h2>
                         <div className="flex items-start justify-between">
-                            <h1 className="font-bold text-xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl 2xl:text-2xl text-gray-500"><CountUp end={totalProductInStock} /></h1>
+                            <h1 className="font-bold text-xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl 2xl:text-2xl text-gray-500"><CountUp end={totalProductInStock} /> các loại.</h1>
                             <div className="p-2 rounded-md bg-red-200 text-2xl"><FaBoxOpen className="text-red-600" /></div>
                         </div>
                     </div>
