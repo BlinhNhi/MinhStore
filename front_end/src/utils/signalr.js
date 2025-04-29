fdc420428e85535ee550873f013fd0db9d129b7e
BlinhNhi <trantoan1682016@gmail.com>
2025-04-29 20:34:05 +0700

    fix UI comment

--- front_end/src/utils/signalr.js
+++ front_end/src/utils/signalr.js
@@ -1,9 +1,15 @@
 import * as signalR from "@microsoft/signalr";
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://localhost:7234/commentHub") // Thay URL nếu khác
-    .withAutomaticReconnect()
-    .build();
+// const connection = new signalR.HubConnectionBuilder()
+//     .withUrl("https://localhost:7234/commentHub") // Thay URL nếu khác
+//     .withAutomaticReconnect()
+//     .build();
 
+// export default connection;
 
-export default connection;
+export const createConnection = (hubEndpoint) => {
+    return new signalR.HubConnectionBuilder()
+        .withUrl(hubEndpoint)
+        .withAutomaticReconnect()
+        .build();
+};
