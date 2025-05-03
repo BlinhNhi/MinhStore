5122cb4f6f89c315f84e0fac578aefed20a5579e
BlinhNhi <trantoan1682016@gmail.com>
2025-05-03 23:29:57 +0700

    feat(comment): delete real-time comment with SignalR

--- front_end/src/components/ActionPopover/ActionPopover.js
+++ front_end/src/components/ActionPopover/ActionPopover.js
@@ -1,4 +1,5 @@
-function ActionPopover(id) {
+function ActionPopover({ onDelete, id, onUpdate }) {
+    console.log('re-render');
     return (
         <div className="absolute right-0 mt-2 w-24 bg-white border rounded shadow z-10">
             <button
@@ -9,7 +10,7 @@ function ActionPopover(id) {
             </button>
             <button
                 className="block w-full px-3 py-2 text-left text-sm hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-500 "
-                onClick={() => console.log("Xoá", id)}
+                onClick={() => onDelete(id)}
             >
                 Xoá
             </button>
