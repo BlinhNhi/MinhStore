bd9bd83f1326c0715b401cf34e66a512c6920fb7
BlinhNhi <trantoan1682016@gmail.com>
2025-05-04 23:14:01 +0700

    feat(comment): implement real-time CRUD via SignalR

--- front_end/src/components/ActionPopover/ActionPopover.js
+++ front_end/src/components/ActionPopover/ActionPopover.js
@@ -1,15 +1,15 @@
-function ActionPopover({ onDelete, id, onUpdate }) {
-    console.log('re-render');
+function ActionPopover({ id, comment, onDelete, onEdit }) {
+    console.log(comment);
     return (
         <div className="absolute right-0 mt-2 w-24 bg-white border rounded shadow z-10">
             <button
-                className="block w-full px-3 py-2 text-left text-sm hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-500"
-                onClick={() => console.log("Sửa", id)}
+                className="block w-full px-3 py-2 text-left text-sm hover:bg-gray-100"
+                onClick={() => onEdit(id, comment)}
             >
                 Sửa
             </button>
             <button
-                className="block w-full px-3 py-2 text-left text-sm hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-500 "
+                className="block w-full px-3 py-2 text-left text-sm hover:bg-gray-100"
                 onClick={() => onDelete(id)}
             >
                 Xoá
